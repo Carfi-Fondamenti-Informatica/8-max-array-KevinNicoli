7395e7411c502de691bbb13fefeb6acc9b297943
KevinNicoli <91669026+KevinNicoli@users.noreply.github.com>
2021-12-11 22:58:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,14 +4,10 @@
 
 float max(int a,float numeri[]) {
     float m=-INFINITY;
-
     for (int i = 0; i < a; i++) {
-
         if (numeri[i]>m){
             m=numeri[i];
         }
-
     }
-
     return m;
 }
